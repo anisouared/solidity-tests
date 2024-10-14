74d6a8e2a5f3b31df1efe782aa6b09fedcefc4e6
Anis O <o.anis@oulook.fr>
2024-10-14 22:12:52 +0200

    Adding contract voting.sol

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # solidity-tests
-Solidity testing project
+
+Solidity testing projects :
+
+# voting.sol :
+
+👉 Le processus de vote :
+
+Voici le déroulement de l'ensemble du processus de vote :
+
+L'administrateur du vote enregistre une liste blanche d'électeurs identifiés par leur adresse Ethereum.
+
+L'administrateur du vote commence la session d'enregistrement de la proposition.
+
+Les électeurs inscrits sont autorisés à enregistrer leurs propositions pendant que la session d'enregistrement est active.
+
+L'administrateur de vote met fin à la session d'enregistrement des propositions.
+
+L'administrateur du vote commence la session de vote.
+
+Les électeurs inscrits votent pour leur proposition préférée.
+
+L'administrateur du vote met fin à la session de vote.
+
+L'administrateur du vote comptabilise les votes.
+
+Tout le monde peut vérifier les derniers détails de la proposition gagnante.
+
+👉 Les recommandations et exigences :
+
+Votre smart contract doit s’appeler “Voting”.
+
+Votre smart contract doit utiliser la dernière version du compilateur.
+
+L’administrateur est celui qui va déployer le smart contract.
+
+Votre smart contract doit définir les structures de données suivantes :
+struct Voter { bool isRegistered; bool hasVoted; uint votedProposalId; } struct Proposal { string description; uint voteCount; }
+
+Votre smart contract doit définir une énumération qui gère les différents états d’un vote
+enum WorkflowStatus { RegisteringVoters, ProposalsRegistrationStarted, ProposalsRegistrationEnded, VotingSessionStarted, VotingSessionEnded, VotesTallied }
+
+Votre smart contract doit définir un uint winningProposalId qui représente l’id du gagnant ou une fonction getWinner qui retourne le gagnant.
+
+Votre smart contract doit importer le smart contract la librairie “Ownable” d’OpenZepplin.
+
+Votre smart contract doit définir les événements suivants :
+event VoterRegistered(address voterAddress);
+event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+event ProposalRegistered(uint proposalId);
+event Voted (address voter, uint proposalId);
---
+++ contracts/voting.sol
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.28;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    struct Voter {
+        bool isRegistred;
+        bool hasVoted;
+        uint votedProporalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    Proposal[] public Proposals;
+
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    uint public winningProposalId;
+    mapping(address => Voter) public Voters;
+    WorkflowStatus status;
+
+    event VoterRegistered(address indexed voterAddress);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint indexed proposalId);
+    event Voted(address indexed voter, uint proposalId);
+
+    modifier onlyVoters() {
+        require(
+            Voters[msg.sender].isRegistred == true,
+            "You are not one of the authorized voters."
+        );
+        _;
+    }
+
+    constructor() Ownable(msg.sender) {}
+
+    function addVoter(address _addr) external onlyOwner {
+        require(
+            status == WorkflowStatus.RegisteringVoters,
+            "This must happen at the very beginning of the process."
+        );
+        require(Voters[_addr].isRegistred != true, "Already registered voter.");
+
+        Voters[_addr] = Voter(true, false, 0);
+
+        emit VoterRegistered(_addr);
+    }
+
+    function startProposalsRegistration() external onlyOwner {
+        require(
+            status == WorkflowStatus.RegisteringVoters,
+            "This must happen during the voter registration step."
+        );
+
+        status = WorkflowStatus.ProposalsRegistrationStarted;
+
+        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, status);
+    }
+
+    function addProposal(
+        string calldata proposalDescription
+    ) external onlyVoters {
+        require(
+            status == WorkflowStatus.ProposalsRegistrationStarted,
+            "This is not the time to make proposals."
+        );
+
+        Proposals.push(Proposal(proposalDescription, 0));
+
+        emit ProposalRegistered(Proposals.length - 1);
+    }
+
+    function stopProposalsRegistration() external onlyOwner {
+        require(
+            status == WorkflowStatus.ProposalsRegistrationStarted,
+            "This must happen when the voters have finished making their proposals."
+        );
+
+        status = WorkflowStatus.ProposalsRegistrationEnded;
+
+        emit WorkflowStatusChange(
+            WorkflowStatus.ProposalsRegistrationStarted,
+            status
+        );
+    }
+
+    function startVoting() external onlyOwner {
+        require(
+            status == WorkflowStatus.ProposalsRegistrationEnded,
+            "This must happen when the proposals registration ended."
+        );
+
+        status = WorkflowStatus.VotingSessionStarted;
+
+        emit WorkflowStatusChange(
+            WorkflowStatus.ProposalsRegistrationEnded,
+            status
+        );
+    }
+
+    function vote(uint _votedProporalId) external onlyVoters {
+        require(
+            status == WorkflowStatus.VotingSessionStarted,
+            "Voting is not yet allowed."
+        );
+        require(
+            _votedProporalId < Proposals.length,
+            "The vote must correspond to the proposals."
+        );
+
+        Voters[msg.sender].votedProporalId = _votedProporalId;
+
+        Proposal memory proposalVoted = Proposals[_votedProporalId];
+        proposalVoted.voteCount += 1;
+        Proposals[_votedProporalId] = proposalVoted;
+
+        emit Voted(msg.sender, _votedProporalId);
+    }
+
+    function stopVoting() external onlyOwner {
+        require(
+            status == WorkflowStatus.VotingSessionStarted,
+            "This must happen during the vote."
+        );
+
+        status = WorkflowStatus.VotingSessionEnded;
+
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, status);
+    }
+
+    function countingFinalVote() external onlyOwner {
+        require(
+            status == WorkflowStatus.VotingSessionEnded,
+            "This must happen at the end of the voting process."
+        );
+
+        winningProposalId = 0; // pas necessaire
+
+        for (uint i = 1; i < Proposals.length; i++) {
+            if (Proposals[i].voteCount > Proposals[i - 1].voteCount) {
+                winningProposalId = i;
+            }
+        }
+
+        status = WorkflowStatus.VotesTallied;
+
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, status);
+    }
+
+    function getWinner() external view returns (Proposal memory) {
+        require(
+            status == WorkflowStatus.VotesTallied,
+            "It must happen after votes tallied."
+        );
+
+        return Proposals[winningProposalId];
+    }
+}
